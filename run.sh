69ab905cd7029b51abf809064201abfaff9f0c7d
Darko Fabijan <darko@renderedtext.com>
2018-11-11 18:17:55 +0100

    Dockerhub test

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
 echo "Command: docker build -t monday-test ."
-
-for i in {1..100}
-do
-   echo "Welcome $i times"
-   docker rmi $(docker images -a -q)
-   docker build -t monday-test .
-done
+docker build -t monday-test .
